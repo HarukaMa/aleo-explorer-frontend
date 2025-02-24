0dd08cee7a5e96fc3f1b2a3ec5322b7981278cfc
Haruka Ma <mrx@hcc.im>
2025-02-24 16:37:48 +0900

    error template for cf

---
+++ src/routes/error/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/state"
+
+  let placeholder = page.url.searchParams.get("p")
+</script>
+
+<style lang="scss">
+  .container {
+    margin: 7.5rem 0;
+  }
+</style>
+
+<div class="container">
+  {placeholder}
+</div>
