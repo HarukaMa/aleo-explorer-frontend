cc0916d4fb78ca013961d20d2c7e92605c20c2c9
Haruka Ma <mrx@hcc.im>
2025-02-24 16:52:45 +0900

    make error page actually usable

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -5,11 +5,44 @@
 </script>
 
 <style lang="scss">
-  .container {
-    margin: 7.5rem 0;
+  @use "/static/styles/variables" as *;
+
+  .error-container {
+    display: flex;
+    flex-direction: column;
+    padding: 7.5rem 0;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    gap: 1rem;
+  }
+
+  .error-code {
+    font-family: "Montserrat Variable", sans-serif;
+    font-size: 6.25rem;
+    font-weight: 600;
+    color: $blue-500;
+  }
+
+  .error-body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .error-title {
+    font-family: "Montserrat Variable", sans-serif;
+    font-weight: 600;
+    line-height: 1.5rem;
+    font-size: 1.125rem;
   }
 </style>
 
-<div class="container">
-  {placeholder}
+<div class="error-container">
+  <div class="error-code">500</div>
+  <div class="error-body">
+    <div class="error-title">Error Title</div>
+    Error Content
+    {placeholder}
+  </div>
 </div>
