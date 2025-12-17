52882639033a75416dc9499ad92e4c7631734803
Vitaliy Irtlach <w13vitaliy@gmail.com>
2025-12-17 20:06:30 +0200

    feat: add redirects

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,37 @@
-import type { HandleServerError } from "@sveltejs/kit"
+import type { Handle, HandleServerError } from "@sveltejs/kit"
+import { redirect } from "@sveltejs/kit"
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const { pathname, searchParams } = event.url
+
+  if (pathname === "/block" && searchParams.has("h")) {
+    const height = searchParams.get("h")
+    redirect(301, `/block/${height}`)
+  }
+
+  if (pathname === "/address" && searchParams.has("a")) {
+    const address = searchParams.get("a")
+    redirect(301, `/address/${address}`)
+  }
+
+  if (pathname === "/program" && searchParams.has("id")) {
+    const id = searchParams.get("id")
+    const edition = searchParams.get("edition") || "1"
+    redirect(301, `/program/${id}/${edition}`)
+  }
+
+  if (pathname === "/transaction" && searchParams.has("id")) {
+    const id = searchParams.get("id")
+    redirect(301, `/transaction/${id}`)
+  }
+
+  if (pathname === "/transition" && searchParams.has("id")) {
+    const id = searchParams.get("id")
+    redirect(301, `/transition/${id}`)
+  }
+
+  return resolve(event)
+}
 
 export const handleError: HandleServerError = async ({ error }) => {
   console.log("server error handler")
